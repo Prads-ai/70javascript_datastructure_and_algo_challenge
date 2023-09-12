ed966f9b8fdae9b24315c5e281cc4a56323a55d1
DPach <63697168+Prads-ai@users.noreply.github.com>
2023-09-12 14:19:07 -0400

    count_occurences.js

--- Basics 101 - Part I/04-count_occurences/count_occurences.js
+++ Basics 101 - Part I/04-count_occurences/count_occurences.js
@@ -1,29 +1,9 @@
-//************************** Fist way to do it **************************************
 count_occurences = (words,char) =>
 {
    return words.split(char).length - 1
 }
-//***********************************************************************
-
-//***************************** Second way to do it *************************************
-count_occurences_II = (words,char) =>
-{
-    count = 0
-    for(let i = 0; i < words.length ; i++)
-    {
-        if (words[i] !== char){
-            continue
-        }
-        else {
-            count++
-        }
-    }
-    return count;
-}
-//******************************************************************************************
-let occurences = count_occurences("hello world",'o')
-let occurences_II = count_occurences_II("hellow world", 'o')
+let occurences = count_occurences("hellow world", 'o')
 console.log(occurences)
-console.log(occurences_II)
+
 
 
