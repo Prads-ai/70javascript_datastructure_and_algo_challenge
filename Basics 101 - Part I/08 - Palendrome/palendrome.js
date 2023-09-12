029c9327b7e8f66f9c957b3ca985176a9a0c4c42
DPach <63697168+Prads-ai@users.noreply.github.com>
2023-09-12 18:44:43 -0400

    palendrome.js
    Clean the code

--- Basics 101 - Part I/08 - Palendrome/palendrome.js
+++ Basics 101 - Part I/08 - Palendrome/palendrome.js
@@ -1,12 +1,12 @@
 isPalindrome = (str) => {
-   let formattedStr =  str.toLowerCase().replace(/[^a-z0-9]/g)
+    let formattedStr =  str.toLowerCase().replace(/[^a-z0-9]/g)
     let reversedFormattedStr = formattedStr.split('').reverse().join('')
     return formattedStr === reversedFormattedStr
 }
+
 isPalindrome_TwoPointer = (str) => {
     left = 0
     right = str.length - 1
-
     while(left < right)
     {
         if(str[left] !== str[right])
@@ -18,15 +18,3 @@ isPalindrome_TwoPointer = (str) => {
     }
     return true
 }
-let a = isPalindrome('racecar');
-let b = isPalindrome('Hello');
-let c = isPalindrome('A man, a plan, a canal, Panama');
-let d = isPalindrome('12321')
-
-let aa = isPalindrome_TwoPointer('racecar');
-let bb = isPalindrome_TwoPointer('Hello');
-let cc = isPalindrome_TwoPointer('A man, a plan, a canal, Panama');
-let dd = isPalindrome_TwoPointer('12321')
-
-console.log(a,b,c,d,)
-console.log(aa,bb,cc,dd)
